fb4b197d9e046d0faba9f9c7109320e61ff11e4b
hitokun-s <jgpuauno@gmail.com>
2016-02-21 03:57:37 +0900

    format

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -9,7 +9,6 @@ from StringIO import StringIO
 import math
 import pylab
 
-
 import chainer
 from chainer import computational_graph
 from chainer import cuda
@@ -21,7 +20,6 @@ from chainer import function
 
 import chainer.functions as F
 import chainer.links as L
-
 
 import numpy
 
@@ -39,23 +37,22 @@ image_dir = './images'
 out_image_dir = './out_images'
 out_model_dir = './out_models'
 
-
-nz = 100          # # of dim for Z
-batchsize=100
-n_epoch=10000
-n_train=200000
+nz = 100  # # of dim for Z
+batchsize = 100
+n_epoch = 10000
+n_train = 200000
 image_save_interval = 50000
-
 
 fs = os.listdir(image_dir)
 print len(fs)
 dataset = []
 for fn in fs:
-    f = open('%s/%s'%(image_dir,fn), 'rb')
+    f = open('%s/%s' % (image_dir, fn), 'rb')
     img_bin = f.read()
     dataset.append(img_bin)
     f.close()
 print len(dataset)
+
 
 # sizeは、1次元なら数字、多次元ならタプル
 def generate_rand(low, hight, size, dtype=np.float32):
@@ -67,9 +64,10 @@ def generate_rand(low, hight, size, dtype=np.float32):
         generated = xp.random.uniform(low, hight, size, ).astype(dtype)
     return generated
 
-class ELU(function.Function):
 
+class ELU(function.Function):
     """Exponential Linear Unit."""
+
     # https://github.com/muupan/chainer-elu
 
     def __init__(self, alpha=1.0):
@@ -80,7 +78,7 @@ class ELU(function.Function):
         x_type, = in_types
 
         type_check.expect(
-            x_type.dtype == numpy.float32,
+                x_type.dtype == numpy.float32,
         )
 
     def forward_cpu(self, x):
@@ -91,8 +89,8 @@ class ELU(function.Function):
 
     def forward_gpu(self, x):
         y = cuda.elementwise(
-            'T x, T alpha', 'T y',
-            'y = x >= 0 ? x : alpha * (exp(x) - 1)', 'elu_fwd')(
+                'T x, T alpha', 'T y',
+                'y = x >= 0 ? x : alpha * (exp(x) - 1)', 'elu_fwd')(
                 x[0], self.alpha)
         return y,
 
@@ -104,8 +102,8 @@ class ELU(function.Function):
 
     def backward_gpu(self, x, gy):
         gx = cuda.elementwise(
-            'T x, T gy, T alpha', 'T gx',
-            'gx = x >= 0 ? gy : gy * alpha * exp(x)', 'elu_bwd')(
+                'T x, T gy, T alpha', 'T gx',
+                'gx = x >= 0 ? gy : gy * alpha * exp(x)', 'elu_bwd')(
                 x[0], gy[0], self.alpha)
         return gx,
 
@@ -116,23 +114,21 @@ def elu(x, alpha=1.0):
     return ELU(alpha=alpha)(x)
 
 
-
-
 class Generator(chainer.Chain):
     def __init__(self):
         super(Generator, self).__init__(
-            l0z = L.Linear(nz, 6*6*512, wscale=0.02*math.sqrt(nz)),
-            dc1 = L.Deconvolution2D(512, 256, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*512)),
-            dc2 = L.Deconvolution2D(256, 128, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*256)),
-            dc3 = L.Deconvolution2D(128, 64, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*128)),
-            dc4 = L.Deconvolution2D(64, 3, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*64)),
-            bn0l = L.BatchNormalization(6*6*512),
-            bn0 = L.BatchNormalization(512),
-            bn1 = L.BatchNormalization(256),
-            bn2 = L.BatchNormalization(128),
-            bn3 = L.BatchNormalization(64),
+                l0z=L.Linear(nz, 6 * 6 * 512, wscale=0.02 * math.sqrt(nz)),
+                dc1=L.Deconvolution2D(512, 256, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 512)),
+                dc2=L.Deconvolution2D(256, 128, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 256)),
+                dc3=L.Deconvolution2D(128, 64, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 128)),
+                dc4=L.Deconvolution2D(64, 3, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 64)),
+                bn0l=L.BatchNormalization(6 * 6 * 512),
+                bn0=L.BatchNormalization(512),
+                bn1=L.BatchNormalization(256),
+                bn2=L.BatchNormalization(128),
+                bn3=L.BatchNormalization(64),
         )
-        
+
     def __call__(self, z, test=False):
         h = F.reshape(F.relu(self.bn0l(self.l0z(z), test=test)), (z.data.shape[0], 512, 6, 6))
         h = F.relu(self.bn1(self.dc1(h), test=test))
@@ -142,23 +138,22 @@ class Generator(chainer.Chain):
         return x
 
 
-
 class Discriminator(chainer.Chain):
     def __init__(self):
         super(Discriminator, self).__init__(
-            c0 = L.Convolution2D(3, 64, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*3)),
-            c1 = L.Convolution2D(64, 128, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*64)),
-            c2 = L.Convolution2D(128, 256, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*128)),
-            c3 = L.Convolution2D(256, 512, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*256)),
-            l4l = L.Linear(6*6*512, 2, wscale=0.02*math.sqrt(6*6*512)),
-            bn0 = L.BatchNormalization(64),
-            bn1 = L.BatchNormalization(128),
-            bn2 = L.BatchNormalization(256),
-            bn3 = L.BatchNormalization(512),
+                c0=L.Convolution2D(3, 64, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 3)),
+                c1=L.Convolution2D(64, 128, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 64)),
+                c2=L.Convolution2D(128, 256, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 128)),
+                c3=L.Convolution2D(256, 512, 4, stride=2, pad=1, wscale=0.02 * math.sqrt(4 * 4 * 256)),
+                l4l=L.Linear(6 * 6 * 512, 2, wscale=0.02 * math.sqrt(6 * 6 * 512)),
+                bn0=L.BatchNormalization(64),
+                bn1=L.BatchNormalization(128),
+                bn2=L.BatchNormalization(256),
+                bn3=L.BatchNormalization(512),
         )
-        
+
     def __call__(self, x, test=False):
-        h = elu(self.c0(x))     # no bn because images from generator will katayotteru?
+        h = elu(self.c0(x))  # no bn because images from generator will katayotteru?
         h = elu(self.bn1(self.c1(h), test=test))
         h = elu(self.bn2(self.c2(h), test=test))
         h = elu(self.bn3(self.c3(h), test=test))
@@ -166,10 +161,8 @@ class Discriminator(chainer.Chain):
         return l
 
 
-
-
 def clip_img(x):
-	return np.float32(-1 if x<-1 else (1 if x>1 else x))
+    return np.float32(-1 if x < -1 else (1 if x > 1 else x))
 
 
 def train_dcgan_labeled(gen, dis, epoch0=0):
@@ -180,83 +173,82 @@ def train_dcgan_labeled(gen, dis, epoch0=0):
     o_gen.add_hook(chainer.optimizer.WeightDecay(0.00001))
     o_dis.add_hook(chainer.optimizer.WeightDecay(0.00001))
 
-    zvis = (generate_rand(-1,1, (100, nz), dtype=np.float32))
+    zvis = (generate_rand(-1, 1, (100, nz), dtype=np.float32))
 
-    for epoch in xrange(epoch0,n_epoch):
+    for epoch in xrange(epoch0, n_epoch):
         perm = np.random.permutation(n_train)
         sum_l_dis = np.float32(0)
         sum_l_gen = np.float32(0)
-        
+
         for i in xrange(0, n_train, batchsize):
             # discriminator
             # 0: from dataset
             # 1: from noise
 
-            #print "load image start ", i
+            # print "load image start ", i
             x2 = np.zeros((batchsize, 3, 96, 96), dtype=np.float32)
             for j in range(batchsize):
                 try:
                     rnd = np.random.randint(len(dataset))
                     rnd2 = np.random.randint(2)
-
-                    img = np.asarray(Image.open(StringIO(dataset[rnd])).convert('RGB')).astype(np.float32).transpose(2, 0, 1)
-                    if rnd2==0:
-                        x2[j,:,:,:] = (img[:,:,::-1]-128.0)/128.0
+                    img = np.asarray(Image.open(StringIO(dataset[rnd])).convert('RGB')).astype(np.float32).transpose(2,0,1)
+                    if rnd2 == 0:
+                        x2[j, :, :, :] = (img[:, :, ::-1] - 128.0) / 128.0
                     else:
-                        x2[j,:,:,:] = (img[:,:,:]-128.0)/128.0
+                        x2[j, :, :, :] = (img[:, :, :] - 128.0) / 128.0
                 except:
                     print 'read image error occured', fs[rnd]
-            #print "load image done"
-            
+            # print "load image done"
+
             # train generator
             z = Variable(generate_rand(-1, 1, (batchsize, nz), dtype=np.float32))
             x = gen(z)
             yl = dis(x)
             L_gen = F.softmax_cross_entropy(yl, Variable(xp.zeros(batchsize, dtype=np.int32)))
             L_dis = F.softmax_cross_entropy(yl, Variable(xp.ones(batchsize, dtype=np.int32)))
-            
+
             # train discriminator
-                    
+
             x2 = Variable(cuda.to_gpu(x2) if using_gpu else x2)
             yl2 = dis(x2)
             L_dis += F.softmax_cross_entropy(yl2, Variable(xp.zeros(batchsize, dtype=np.int32)))
-            
-            #print "forward done"
+
+            # print "forward done"
 
             o_gen.zero_grads()
             L_gen.backward()
             o_gen.update()
-            
+
             o_dis.zero_grads()
             L_dis.backward()
             o_dis.update()
-            
+
             sum_l_gen += L_gen.data.get()
             sum_l_dis += L_dis.data.get()
-            
-            #print "backward done"
 
-            if i%image_save_interval==0:
-                pylab.rcParams['figure.figsize'] = (16.0,16.0)
+            # print "backward done"
+
+            if i % image_save_interval == 0:
+                pylab.rcParams['figure.figsize'] = (16.0, 16.0)
                 pylab.clf()
                 vissize = 100
                 z = zvis
-                z[50:,:] = (xp.random.uniform(-1, 1, (50, nz), dtype=np.float32))
+                z[50:, :] = (xp.random.uniform(-1, 1, (50, nz), dtype=np.float32))
                 z = Variable(z)
                 x = gen(z, test=True)
                 x = x.data.get()
                 for i_ in range(100):
-                    tmp = ((np.vectorize(clip_img)(x[i_,:,:,:])+1)/2).transpose(1,2,0)
-                    pylab.subplot(10,10,i_+1)
+                    tmp = ((np.vectorize(clip_img)(x[i_, :, :, :]) + 1) / 2).transpose(1, 2, 0)
+                    pylab.subplot(10, 10, i_ + 1)
                     pylab.imshow(tmp)
                     pylab.axis('off')
-                pylab.savefig('%s/vis_%d_%d.png'%(out_image_dir, epoch,i))
-                
-        serializers.save_hdf5("%s/dcgan_model_dis_%d.h5"%(out_model_dir, epoch),dis)
-        serializers.save_hdf5("%s/dcgan_model_gen_%d.h5"%(out_model_dir, epoch),gen)
-        serializers.save_hdf5("%s/dcgan_state_dis_%d.h5"%(out_model_dir, epoch),o_dis)
-        serializers.save_hdf5("%s/dcgan_state_gen_%d.h5"%(out_model_dir, epoch),o_gen)
-        print 'epoch end', epoch, sum_l_gen/n_train, sum_l_dis/n_train
+                pylab.savefig('%s/vis_%d_%d.png' % (out_image_dir, epoch, i))
+
+        serializers.save_hdf5("%s/dcgan_model_dis_%d.h5" % (out_model_dir, epoch), dis)
+        serializers.save_hdf5("%s/dcgan_model_gen_%d.h5" % (out_model_dir, epoch), gen)
+        serializers.save_hdf5("%s/dcgan_state_dis_%d.h5" % (out_model_dir, epoch), o_dis)
+        serializers.save_hdf5("%s/dcgan_state_gen_%d.h5" % (out_model_dir, epoch), o_gen)
+        print 'epoch end', epoch, sum_l_gen / n_train, sum_l_dis / n_train
 
 
 gen = Generator()
@@ -265,7 +257,6 @@ dis = Discriminator()
 if using_gpu:
     gen.to_gpu()
     dis.to_gpu()
-
 
 try:
     os.mkdir(out_image_dir)
