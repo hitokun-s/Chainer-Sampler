b6623623cd456b40c710ea3d5adc8e54140efaca
hitokun-s <jgpuauno@gmail.com>
2016-02-25 23:10:19 +0900

    ゴミデータを捨てる工夫を追加

--- catch-up/nltk_test.py
+++ catch-up/nltk_test.py
@@ -27,4 +27,60 @@ def process(sentences):
         result.append(new_review)
     return result
 
+from nltk.corpus import wordnet
+
+from functools import wraps
+
+def unicode_ignore_invalid_char(text):
+    if isinstance(text, str):
+        return text.decode('utf-8', 'ignore')
+    return text
+
+def str_ignore_invalid_char(text):
+    if isinstance(text, unicode):
+        return text.encode('utf-8', 'ignore')
+    return text
+
+def consistent_texttype(function):
+    @wraps(function)
+    def _consistent_texttype(*args, **kwargs):
+        assert(1 <= len(args))
+        input_text = args[0]
+        is_unicode = False
+        if isinstance(input_text, unicode):
+            is_unicode = True
+        elif not isinstance(input_text, str):
+            is_unicode = isinstance(input_text[0], unicode)  # for collections
+        output_text = function(*args, **kwargs)
+        if isinstance(output_text, unicode) or isinstance(output_text, str):
+            if is_unicode:
+                return unicode_ignore_invalid_char(output_text)
+            return str_ignore_invalid_char(output_text)
+        if is_unicode:
+            return map(unicode_ignore_invalid_char, output_text)
+        return map(str_ignore_invalid_char, output_text)
+    return _consistent_texttype
+
+@consistent_texttype
+def lemmatize_term(term, pos=None):
+    if pos is None:
+        synsets = wordnet.synsets(term)
+        if not synsets:
+            return term
+        pos = synsets[0].pos
+        if pos == wordnet.ADJ_SAT:
+            pos = wordnet.ADJ
+    assert(pos in (wordnet.NOUN, wordnet.VERB, wordnet.ADJ, wordnet.ADV))
+    return nltk.WordNetLemmatizer().lemmatize(term, pos=pos)
+
+lemmatizer =nltk.WordNetLemmatizer()
+
+
 print process(["I am a dog, who is running on the ground, with a flying sourcer."])
+
+print lemmatizer.lemmatize('men')
+print wordnet.lemmas('men')
+
+print lemmatizer.lemmatize('1234')
+assert len(wordnet.lemmas('1234')) == 0
+assert len(wordnet.synsets('1234')) == 0
--- word2vec/prepare_data.py
+++ word2vec/prepare_data.py
@@ -5,6 +5,37 @@ import numpy
 import six
 import collections, pickle, os
 
+import nltk, re, string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from functools import wraps
+
+# nltk.download() <= 1回だけ実行してダウンロードしておけばOK
+
+# stop word list
+stop_words = nltk.corpus.stopwords.words('english')
+print stop_words # 人称代名詞、be動詞、疑問詞、助動詞、前置詞、などなど
+
+regex = re.compile('[%s]' % re.escape(string.punctuation)) #see documentation here: http://docs.python.org/2/library/string.html
+
+lemmatizer =nltk.WordNetLemmatizer()
+
+def process(sentences):
+    result = []
+    tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+    for review in tokenized_sentences:
+        new_review = []
+        for token in review:
+            new_token = regex.sub(u'', token)
+            if not new_token == u'' and not new_token in stop_words:
+                if len(wordnet.lemmas(new_token)) > 0:
+                    new_review.append(lemmatizer.lemmatize(new_token))
+            # new_token = regex.sub('', token)
+            # if not new_token == '' and not new_token in stop_words:
+            #     new_review.append(new_token)
+        result.append(new_review)
+    return result
+
 curr_dir = os.path.dirname(__file__)
 
 def doExec():
@@ -17,8 +48,12 @@ def doExec():
     # 文書全体を単語IDリストに変換したもの
     dataset = []
 
-    def process_line(line):
-        for word in line.split(): # スペースで分割
+    def process_line(contents):
+        res = process(contents.split(".")) # resは、配列の配列になる
+        words = []
+        for r in res:
+            words.extend(r)
+        for word in words:
             # 新出単語だったら、順引辞書、逆引辞書に登録
             if word not in word2index:
                 ind = len(word2index)
@@ -43,7 +78,13 @@ def doExec():
                     data = ""
                     cnt = cnt + 1
                     continue
-                process_line(line)
+                try:
+                    process_line(line.encode('utf-8', 'ignore'))
+                except:
+                    pass
+                    # print "utf-8 encode error!"
+                    # print line
+
 
     print len(dataset)
     print len(index2word)
@@ -63,5 +104,13 @@ def doExec():
 
 if __name__ == '__main__':
     f = open(curr_dir + "/data/pickle.dump", "w")
-    pickle.dump(doExec(), f)
+    data = doExec()
+    print "go!"
+    print len(data["word2index"])
+    # 何も工夫をしないと、englishText_0_10000ファイルだけで17万語彙になってしまったが、その多くは、数字とか固有名詞というゴミデータだった
+    # そこで、lemmatizeを行うとともに、wordnetに見出し語が存在しない語を捨てたら、語彙数は48000になった。
+    pickle.dump(data, f)
     f.close()
+
+    for w in data["word2index"]:
+        print w
