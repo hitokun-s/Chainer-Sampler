6202dc18443c9ae855150c247fe52ce50681acea
hitokun-s <jgpuauno@gmail.com>
2016-03-26 16:54:37 +0200

    bugfix

--- imagenet/multi_resize.sh
+++ imagenet/multi_resize.sh
@@ -7,7 +7,10 @@ i=1
 for imgfile in $( ls images ); do
     echo $i
     echo images/$imggile
-    mogrify -resize 256x256 images/${imgfile}
+#    これだと長軸編が256になるだけで、リサイズになって、正方形になってくれない
+#    mogrify -resize 256x256 images/${imgfile}
+# refs: http://blog.knazsky.com/post/102867716507/create-square-thumbnails-with-imagemagic
+    mogrify -thumbnail 256x256 -gravity center -crop 256x256+0+0 +repage -format jpg -quality 75 images/${imgfile}
 
     # iを$i+1で上書きする
     i=$((i+1))
