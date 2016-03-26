ad61dfd04c55e738b598262c2b461a7572794a42
hitokun-s <jgpuauno@gmail.com>
2016-03-26 15:39:06 +0200

    mod shell

--- imagenet/multi_resize.sh
+++ imagenet/multi_resize.sh
@@ -1,3 +1,15 @@
+#!/usr/bin/env bash
 echo start!
-mogrify -resize 256x256 images/*.jpg
+# mogrify -resize 256x256 images/*.jpg
+# これだとargument list too long になってしまう
+
+i=1
+for imgfile in $( ls images ); do
+    echo $i
+    mogrify -resize 256x256 "images/${imggile}"
+
+    # iを$i+1で上書きする
+    i=$((i+1))
+done
+
 pause
