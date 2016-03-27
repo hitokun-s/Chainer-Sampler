315ea3d726d02eac708134976d64a3a462d3cd36
hitokun-s <jgpuauno@gmail.com>
2016-03-27 15:20:26 +0300

    bugfix

--- imagenet/square_max.py
+++ imagenet/square_max.py
@@ -11,9 +11,9 @@ for i, imgpath in enumerate(os.listdir("images")):
         continue
     print i
     img = Image.open("images/" + imgpath)
-    size = min(img.height, img.width)
-    start_x = img.width / 2 - size / 2
-    start_y = img.height / 2 - size / 2
+    size = min(img.size)
+    start_x = img.size[0] / 2 - size / 2
+    start_y = img.size[1] / 2 - size / 2
     box = (start_x, start_y, start_x + size, start_y + size) #  box is a 4-tuple defining the left, upper, right, and lower pixel coordinate.
     img2 = img.crop(box).resize((256, 256), Image.ANTIALIAS)
     img2.save('images/' + imgpath, 'JPEG')
